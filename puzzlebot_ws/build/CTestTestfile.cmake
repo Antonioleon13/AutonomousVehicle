e6fd70b5194fe3567d38ac2ef9950d16aa314818
Jose Antonio <a01639250@tec.mx>
2023-10-30 01:40:05 +0000

    Final project

---
+++ puzzlebot_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jose/workspace/AutonomousVehicle/puzzlebot_ws/src
+# Build directory: /home/jose/workspace/AutonomousVehicle/puzzlebot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ puzzlebot_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jose/workspace/AutonomousVehicle/puzzlebot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/jose/workspace/AutonomousVehicle/puzzlebot_ws/devel;/home/jose/workspace/crxrobot_ws/devel;/opt/ros/noetic'.split(';') if '/home/jose/workspace/AutonomousVehicle/puzzlebot_ws/devel;/home/jose/workspace/crxrobot_ws/devel;/opt/ros/noetic' != '' else []
